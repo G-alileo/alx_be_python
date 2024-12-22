688ed19fa3b08923083000ff3466ff67f92e6000
G-alileo <jamespmurithi@gmail.com>
2024-12-22 07:38:20 +0300

    Modified multiplication_table.py file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,5 +3,6 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 # Generate and Print the Multiplication Table:
 for Y in range(1,11):
-    print("%d x %d = %d" %(number , Y, (number * Y)))
+    Z = "%d * %d = %d" %(number , Y, (number * Y))
+    print(Z)
 

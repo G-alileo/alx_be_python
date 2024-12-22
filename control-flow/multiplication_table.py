50b8f9d6cc580deb8e7d2c14d1a85c5f86646d9e
G-alileo <jamespmurithi@gmail.com>
2024-12-22 06:54:12 +0300

    Addition of multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,4 @@
-# Prompt User for a Number:
 
-# Ask the user to input a number for which they want to see the multiplication table
-# : Enter a number to see its multiplication table:.
-# Save it in a variable name number
 # Generate and Print the Multiplication Table:
 
 # Use a for loop to iterate through the numbers 1 to 10.
@@ -10,3 +6,11 @@
 # and the iterator (the current number in the loop from 1 to 10).
 # Print each line of the multiplication table in the format: “X * Y = Z”, 
 # where X is the user’s number, Y is the current number in the loop, and Z is the product.
+
+# Prompt User for a Number:
+number = int(input("Enter a number to see its multiplication table:"))
+
+# Generate and Print the Multiplication Table:
+for Y in range(1,11):
+    print("%d * %d = %d" %(number , Y, (number * Y)))
+

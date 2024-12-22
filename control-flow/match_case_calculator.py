c4399a166b3ae547b0aa740fe59551eb482388ba
G-alileo <jamespmurithi@gmail.com>
2024-12-22 06:44:01 +0300

    Addition of match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,4 @@
 # Prompt for User Input:
-
-# Ask the user to input two numbers (one at a time) using: Enter the first number: and Enter the second number:
-# Make sure you use num1 and num2 for first and second numbers
-# Ask for the type of operation they’d like to perform: Choose the operation (+, -, *, /):.
-# Perform the Calculation Using Match Case:
-
-# Implement a Match Case statement that executes the chosen operation based on the user’s input.
-# For addition (+), subtract (-), multiply (*), and divide (/).
-# Ensure to handle the division by zero case gracefully, displaying a message if the user tries to divide by zero.
-# Output the Result:
-
-# Display the result of the operation in a user-friendly message, e.g., The result is [result].
-
-# Prompt for User Input:
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 operation = input("Choose the operation (+, -, *, /):")
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,12 @@
+# Prompt User for a Number:
+
+# Ask the user to input a number for which they want to see the multiplication table
+# : Enter a number to see its multiplication table:.
+# Save it in a variable name number
+# Generate and Print the Multiplication Table:
+
+# Use a for loop to iterate through the numbers 1 to 10.
+# For each iteration, calculate the product of the user’s number 
+# and the iterator (the current number in the loop from 1 to 10).
+# Print each line of the multiplication table in the format: “X * Y = Z”, 
+# where X is the user’s number, Y is the current number in the loop, and Z is the product.

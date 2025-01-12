e2ab7c90ca1aecc0ab70d5bb6f16fdbb73623d27
G_alileo <jamespmurithi@gmail.com>
2025-01-12 16:54:20 +0300

    Addition of fns_and_dsa directory

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,18 +2,17 @@ def perform_operation(num1, num2, operation):
     oper = ("add", "subtract", "multiply", "divide")
     validity = True if operation in oper else False
     
-    if validity == True:
-        match operation:
-            case "add":
-                return num1 + num2
-            case "subtract":
-                return num1 - num2
-            case "multiply":
-                return num1 * num2
-            case "divide":
-                if num2 != 0:
+    if num2 == 0 and operation == "divide":
+            return "Error: can't divide by zero"
+    elif validity == True:
+            match operation:
+                case "add":
+                    return num1 + num2
+                case "subtract":
+                    return num1 - num2
+                case "multiply":
+                    return num1 * num2
+                case "divide":
                     return num1 / num2
-                else:
-                    return "Error : can,t divide by zero"
-            
+                
     return "Error: Invalid operation"
